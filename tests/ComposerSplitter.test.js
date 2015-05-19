2be004945386de26173048bfca60a897b8f40802
Àlex Fiestas <afiestas@kde.org>
2015-05-19 08:16:37 +0200

    Implement ComposerSplitter
    This class is in charge of splitting the docker-compose log at least in
    two parts:
    component: Which component this line belongs to
    line: The actual log

---
+++ tests/ComposerSplitter.test.js
@@ -0,0 +1,26 @@
+var sinon = require('sinon');
+var assert = require('chai').assert;
+var ComposerSplitter = require('../lib/ComposerSplitter');
+
+suite('ComposerSplitter', function(){
+    var sut;
+
+    var component = 'authentication_1';
+    var line = '[2015-05-16 14:22:20.744] [DEBUG] MongoPersistence';
+    var str = component + ' | ' + line;
+    setup(function(){
+        sut = new ComposerSplitter();
+    });
+
+    suite('#split', function(){
+        test('Should return an object with component and line', function(){
+            var r = sut.split(str);
+            assert.deepEqual(r, {component: component, line: line});
+        });
+    });
+});
+
+
+
+
+
