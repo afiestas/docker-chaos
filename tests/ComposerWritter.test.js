07da350bac5c279ed0d6a760ead8ae7b784a5631
Àlex Fiestas <afiestas@kde.org>
2015-05-19 08:56:57 +0200

    Finish implementation of ComposerWritter.write
    The only remaining part is to forward some path where logs will be
    created and stored.

--- tests/ComposerWritter.test.js
+++ tests/ComposerWritter.test.js
@@ -4,19 +4,40 @@ var ComposerSplitter = require('../lib/ComposerSplitter');
 var ComposerWritter = require('../lib/ComposerWritter');
 
 suite('ComposerWritter', function(){
-    var sut, splitter;
+    var sut, splitter, fs, fd;
 
+    var component = 'component_1';
     var line = 'some debug line';
 
+    var lineInfo = {component: component, line: line};
+
     setup(function(){
+        fd = 1;
+        fs = {open: sinon.stub(), write: sinon.stub()};
         splitter = sinon.stub(new ComposerSplitter());
-        sut = new ComposerWritter(splitter);
+        splitter.split.returns(lineInfo);
+
+        sut = new ComposerWritter(splitter, fs);
     });
 
     suite('#write', function(){
         test('Should call this.splitter.split forwarding the line', function(){
             sut.write(line);
             sinon.assert.calledWith(splitter.split, line);
+        });
+        test('Should create a fileStream for the component log is for', function(){
+            sut.write(line);
+            sinon.assert.calledWith(fs.open, '/tmp/' + lineInfo.component, 'a');
+        });
+        test('Should write to fd returned by fs.open', function(){
+            fs.open.yields(null, fd);
+            sut.write(line);
+            sinon.assert.calledWith(fs.write, fd, line);
+        });
+        test('Should not call open if we have a fd for that component', function(){
+            sut.fd[component] = 1;
+            sut.write(line);
+            sinon.assert.notCalled(fs.open);
         });
     });
 });
