cbf55066009404b0995812dc4d8f373b0ac3deca
Àlex Fiestas <afiestas@kde.org>
2015-05-19 07:44:34 +0200

    Adding ComposerProcess class
    This class will be in charge of knowing how to talk with the docker-
    composer log system and emit a line event every time any new log appear.

---
+++ tests/ComposerProcess.test.js
@@ -0,0 +1,40 @@
+var sinon = require('sinon');
+var assert = require('chai').assert;
+var EventEmitter = require('events').EventEmitter;
+var ComposerProcess = require('../lib/ComposerProcess.js');
+
+suite('ComposerProcess', function(){
+    var sut, childProcess, dockerComposeProc, byline, stream;
+
+    setup(function(){
+        stream = new EventEmitter();
+        byline = sinon.stub();
+        byline.returns(stream);
+
+        dockerComposeProc = {stdout: 'setdout'};
+        childProcess = {
+            spawn: sinon.stub()
+        };
+        childProcess.spawn.returns(dockerComposeProc);
+        sut = new ComposerProcess(childProcess, byline);
+    });
+
+    suite('#start', function(){
+        test('Should call this.spawn with correct command', function(){
+            sut.start();
+            sinon.assert.calledWithExactly(childProcess.spawn, 'docker-compose', ['logs']);
+        });
+        test('Should connect stdout from spawn to byline', function(){
+            sut.start();
+            sinon.assert.calledWithExactly(byline, dockerComposeProc.stdout);
+        });
+        test('Should emit line whenever returned stream data event is emitted', function(done){
+            sut.on('line', done);
+            sut.start();
+            stream.emit('data');
+        });
+    });
+});
+
+
+
